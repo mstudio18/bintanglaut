ec40fb2a29893837278c4b3d56093ead8b4df47f
Luke Thurai <mukhee.corp@gmail.com>
2019-12-14 14:15:24 +0700

    first deploy

---
+++ application/controllers/Artikel.php
@@ -0,0 +1,177 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Artikel extends CI_Controller {
+
+  public function __construct()
+  {
+      parent::__construct();
+      $this->load->model('artikel_model');
+      $this->load->library('upload');
+      $this->load->library('pagination');
+  }
+
+  // fungsi untuk mengambil data
+	public function index()
+	{
+
+      $cari = $this->input->get('cari');
+      $page = $this->input->get('per_page');
+
+      $search = array('judul' => $cari );
+
+      $batas =  9; // 9 data per page
+      if(!$page):
+          $offset = 0;
+      else:
+          $offset = $page;
+      endif;
+
+      $config['page_query_string'] = TRUE;
+  		$config['base_url'] 				 = base_url().'index.php/artikel/?cari='.$cari;
+  		$config['total_rows'] 			 = $this->artikel_model->jumlah_row($search);
+
+  		$config['per_page'] 				 = $batas;
+  		$config['uri_segment'] 			 = $page;
+
+  		$config['full_tag_open'] 		= '<ul class="pagination">';
+  		$config['full_tag_close'] 	= '<ul>';
+
+  		$config['first_link'] 			= 'first';
+  		$config['first_tag_open'] 	= '<li><a>';
+  		$config['first_tag_close'] 	= '</a></li>';
+
+  		$config['last_link'] 				= 'last';
+  		$config['last_tag_open']	 	= '<li><a>';
+  		$config['last_tag_close'] 	= '</a></li>';
+
+  		$config['next_link'] 				= '&raquo;';
+  		$config['next_tag_open'] 		= '<li><a>';
+  		$config['next_tag_close'] 	= '</a></li>';
+
+  		$config['prev_link'] 				= '&laquo;';
+  		$config['prev_tag_open'] 		= '<li><a>';
+  		$config['prev_tag_close'] 	= '</a></li>';
+
+  		$config['cur_tag_open'] 		= '<li class="active"><a>';
+  		$config['cur_tag_close'] 		= '</a></li>';
+
+  		$config['num_tag_open'] 		= '<li><a>';
+  		$config['num_tag_close'] 		= '</a></li>';
+
+      $this->pagination->initialize($config);
+      $data['pagination']	 = $this->pagination->create_links();
+      $data['jumlah_page'] = $page;
+
+
+      $data['data'] = $this->artikel_model->get($batas,$offset,$search);
+
+  		$this->load->view('artikel',$data);
+	}
+
+  // untuk menampilkan halaman tambah data
+  public function tambah()
+  {
+      return $this->load->view('tambah_data');
+  }
+
+  // untuk memasukan data ke database
+  public function insertdata()
+  {
+      $judul   = $this->input->post('judul');
+      $artikel = $this->input->post('artikel');
+
+      // get foto
+      $config['upload_path'] = './assets/picture';
+      $config['allowed_types'] = 'jpg|png|jpeg|gif';
+      $config['max_size'] = '2048';  //2MB max
+      $config['max_width'] = '4480'; // pixel
+      $config['max_height'] = '4480'; // pixel
+      $config['file_name'] = $_FILES['fotopost']['name'];
+
+      $this->upload->initialize($config);
+
+	    if (!empty($_FILES['fotopost']['name'])) {
+	        if ( $this->upload->do_upload('fotopost') ) {
+	            $foto = $this->upload->data();
+	            $data = array(
+	                          'judul'       => $judul,
+                            'foto'       => $foto['file_name'],
+	                          'artikel'     => $artikel,
+	                        );
+							$this->artikel_model->insert($data);
+              redirect('');
+	        }else {
+              die("gagal upload");
+	        }
+	    }else {
+	      echo "tidak masuk";
+	    }
+
+  }
+
+  // delete
+  public function deletedata($id,$foto)
+  {
+      $path = './assets/picture/';
+      @unlink($path.$foto);
+
+      $where = array('id' => $id );
+      $this->artikel_model->delete($where);
+      return redirect('');
+  }
+
+  // edit
+  public function edit($id)
+  {
+      $kondisi = array('id' => $id );
+
+      $data['data'] = $this->artikel_model->get_by_id($kondisi);
+      return $this->load->view('edit_data',$data);
+  }
+
+  // update
+  public function updatedata()
+  {
+      $id   = $this->input->post('id');
+      $name = $this->input->post('judul');
+      $alamat = $this->input->post('artikel');
+
+      $path = './assets/picture/';
+
+      $kondisi = array('id' => $id );
+
+      // get foto
+      $config['upload_path'] = './assets/picture';
+      $config['allowed_types'] = 'jpg|png|jpeg|gif';
+      $config['max_size'] = '2048';  //2MB max
+      $config['max_width'] = '4480'; // pixel
+      $config['max_height'] = '4480'; // pixel
+      $config['file_name'] = $_FILES['fotopost']['name'];
+
+      $this->upload->initialize($config);
+
+	    if (!empty($_FILES['fotopost']['name'])) {
+	        if ( $this->upload->do_upload('fotopost') ) {
+	            $foto = $this->upload->data();
+	            $data = array(
+	                          'judul'       => $judul,
+                            'foto'       => $foto['file_name'],
+	                          'artikel'     => $artikel,
+	                        );
+              // hapus foto pada direktori
+              @unlink($path.$this->input->post('filelama'));
+
+							$this->artikel_model->update($data,$kondisi);
+              redirect('');
+	        }else {
+              die("gagal update");
+	        }
+	    }else {
+	      echo "tidak masuk";
+	    }
+
+  }
+
+
+} // end class
---
+++ application/views/artikel.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>BintangLaut</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="<?=base_url()?>assets/font/glyphicons-halflings-regular.ttf">
+
+  </head>
+  <body>
+    <div class="container">
+      <h1>BintangLaut.co.id</h1>
+      <hr>
+      <a href="<?=base_url()?>index.php/home/tambah" class="btn btn-success">+ Artikel</a>
+    </div>
+
+    <div class="container">
+      <h2>Daftar Artikel</h2>
+      <form action="<?=base_url()?>index.php/home/index" method="get">
+        <input type="text" name="cari">
+        <input type="submit" value="Cari" class="btn btn-default">
+      </form>
+      <div class="row">
+
+        <?php foreach ($data as $data): ?>
+          <div class="col-sm-6 col-md-3">
+            <a href="#" class="thumbnail">
+              <img src="<?=base_url()?>assets/picture/<?=$data->foto?>" alt="foto">
+            </a>
+            <div class="caption">
+              <h3><?php echo $data->judul?></h3>
+              <p>Artikel: <?php echo substr($data->artikel, 0 , 250) ?></p>
+              <p>
+                <a href="<?=base_url()?>index.php/home/edit/<?=$data->id?>" class="btn btn-info" role="button">Edit</a>
+                <a href="<?=base_url()?>index.php/home/deletedata/<?=$data->id?>/<?=$data->foto?>" class="btn btn-danger" role="button">Hapus</a>
+              </p>
+            </div>
+          </div>
+        <?php endforeach; ?>
+
+
+      </div>
+    </div>
+    <div class="container">
+      <?php echo $pagination ?>
+    </div>
+
+    <script src="<?= base_url() ?>assets/js/jquery.min.js"></script>
+    <script src="<?= base_url() ?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html>
