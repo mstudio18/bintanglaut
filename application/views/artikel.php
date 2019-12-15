c9430ba0071cc6fa94955c56a6eb04605b044628
Ahmad Syarif Hidayat <ahmadsyarifhidayat23@gmail.com>
2019-12-15 18:45:11 +0700

    update template adminlte

--- application/views/artikel.php
+++ application/views/artikel.php
@@ -1,16 +1,26 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>BintangLaut</title>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>AdminLTE 3 | Project Edit</title>
+	<!-- Tell the browser to be responsive to screen width -->
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="<?=base_url()?>assets/font/glyphicons-halflings-regular.ttf">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?=base_url()?>plugins/fontawesome-free/css/all.min.css">
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+	<!-- overlayScrollbars -->
+	<link rel="stylesheet" href="<?=base_url()?>dist/css/adminlte.min.css">
+	<!-- Google Font: Source Sans Pro -->
+	<link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 
-  </head>
+	<!-- Bootstrap >
+	<link rel="stylesheet" href="<?=base_url()?>assets/css/bootstrap.min.css">
+	<link rel="stylesheet" href="<?=base_url()?>assets/font/glyphicons-halflings-regular.ttf"-->
+
+</head>
   <body>
 <!-- test repo tambahan buat liat perubahan-->
     <div class="container">
