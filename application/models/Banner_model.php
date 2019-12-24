ab4fa5578b7bf9e8db6c27d2cc8fb33c5f3146de
Luke Thurai <mukhee.corp@gmail.com>
2019-12-25 00:34:54 +0700

    update banner

---
+++ application/models/Banner_model.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ *
+ */
+class Banner_model extends CI_Model
+{
+
+  public function get($batas=NULL,$offset=NULL,$cari=NULL)
+  {
+      if ($batas != NULL) {
+        $this->db->limit($batas,$offset);
+      }
+      if ($cari != NULL) {
+          $this->db->or_like($cari);
+      }
+      $this->db->from('banner');
+      $query = $this->db->get();
+      return $query->result();
+  }
+  public function jumlah_row($search)
+  {
+    $this->db->or_like($search);
+    $query = $this->db->get('banner');
+
+    return $query->num_rows();
+  }
+
+  public function get_by_id($kondisi)
+  {
+      $this->db->from('banner');
+      $this->db->where($kondisi);
+      $query = $this->db->get();
+      return $query->row();
+  }
+
+  public function insert($data)
+  {
+      $this->db->insert('banner',$data);
+      return TRUE;
+  }
+  public function delete($where)
+  {
+      $this->db->where($where);
+      $this->db->delete('banner');
+      return TRUE;
+  }
+  public function update($data,$kondisi)
+  {
+      $this->db->update('banner',$data,$kondisi);
+      return TRUE;
+  }
+
+}
